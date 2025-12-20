e800a476c309f5fb23c5fa20bcb81755be8c2a85
Richard Howard <richard.howard@wonderschool.com>
2025-12-20 06:26:57 -0500

    flatten, merge, join

--- lib/tailwind_merge.ex
+++ lib/tailwind_merge.ex
@@ -10,5 +10,24 @@ defmodule TailwindMerge do
   """
   def tw(classes) do
     classes
+    |> flatten()
+    |> merge()
+    |> join()
+  end
+
+  defp merge(classes), do: classes
+
+  defp flatten(nil), do: []
+  defp flatten(false), do: []
+  defp flatten({_class, nil}), do: []
+  defp flatten({_class, false}), do: []
+  defp flatten({class, true}), do: [class]
+  defp flatten(s) when is_binary(s), do: [String.trim(s)]
+  defp flatten(lst) when is_list(lst), do: Enum.flat_map(lst, &flatten/1)
+
+  defp join(classes) do
+    classes
+    |> Enum.join(" ")
+    |> String.trim()
   end
 end
--- test/tailwind_merge_test.exs
+++ test/tailwind_merge_test.exs
@@ -7,5 +7,9 @@ defmodule TailwindMergeTest do
 
   test "foo" do
     assert tw("bg-red-500") == "bg-red-500"
+    assert tw(["bg-red-500"]) == "bg-red-500"
+    assert tw([nil, "bg-red-500", [false]]) == "bg-red-500"
+    assert tw(["", [block: false]]) == ""
+    assert tw(["", [block: true]]) == "block"
   end
 end
